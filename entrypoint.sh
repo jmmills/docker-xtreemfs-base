43ef0ddf6bf4f90bcf13f2eba7d8f0fd3953b381
Jason Mills <jason@yarp.me>
2014-08-20 12:44:17 -0700

    print out dir host

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,5 +2,7 @@
 
 CLASSPATH=/usr/share/java/XtreemFS.jar:/usr/share/java/BabuDB.jar:/usr/share/java/Flease.jar:/usr/share/java/protobuf-java-2.5.0.jar:/usr/share/java/Foundation.jar:/usr/share/java/jdmkrt.jar:/usr/share/java/jdmktk.jar:/usr/share/java/commons-codec-1.3.jar
 perl -pi /fix_config.pl $2
+grep dir_service.host $2
+grep dir_service.port $2
 /usr/bin/java -ea -cp $CLASSPATH $1 $2
 
